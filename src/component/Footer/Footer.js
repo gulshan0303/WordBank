7f813a8eea41cf3e5cdbfc829c1e15608d0d212f
Gulshan kathare <gulshan03kathare@gmail.com>
2022-01-30 14:56:32 +0530

    word hunt

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return  <div className='footer'>
+    Made with ❤️ by{" "}
+    <a className="name"
+      href="https://www.linkedin.com/in/gulshan-kathare-49945b213/ "  target="__blank"
+    
+    >
+     <h3>Gulshan Kathare</h3>
+    </a>
+  </div>;
+};
+
+export default Footer;
